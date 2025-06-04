f12d8837af70417d1c6f5f77c514cf8b3d606c0a
Anonymous-2003 <avishekbasyal3@gmail.com>
2025-06-04 19:20:19 +0545

    taking photos on ios only after managing permission by the user

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,12 +1,40 @@
-import { launchCameraAsync } from "expo-image-picker"; // to open camera by the user
-import { Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
 
 const ImagePicker = () => {
+  const [cameraPermissioiinInformation, requestPermission] = useCameraPermissions();
+  //cameraPermissionInformation: the current permission state
+  //requestPermission: a function to request permission from the user
+
+  async function verifyPermissions() {
+    if (
+      cameraPermissioiinInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (cameraPermissioiinInformation.status === PermissionStatus.DENIED) {
+      Alert.alert("Permission not allowed", "You need to allow camera access");
+      return false;
+    }
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
-      quality:0.5
+      quality: 0.5,
     });
     console.log(image);
   }
